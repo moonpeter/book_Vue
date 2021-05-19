0ccc164ca284df21f4e044699a82afcf931b7556
moonpeter <moonpeter@kakao.com>
2021-05-19 23:46:35 +0900

    [5/19] p.147

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>input</div>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">추가</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "TodoInput"
+  data() {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value, value);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
